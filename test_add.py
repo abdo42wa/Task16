73e55b2414e62eb1075f2803dfe8868f9a1a01d6
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2024-05-14 20:15:13 +0300

    extend the pipeline to include running unit tests in addition to linting

---
+++ test_add.py
@@ -0,0 +1,23 @@
+import unittest
+
+def add(a, b):
+    return a + b
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(1, 2), 3)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -2), -3)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(10, -5), 5)
+
+    def test_add_zero(self):
+        self.assertEqual(add(0, 0), 0)
+
+    def test_add_floats(self):
+        self.assertAlmostEqual(add(0.1, 0.2), 0.3)
+
+if __name__ == '__main__':
+    unittest.main()
